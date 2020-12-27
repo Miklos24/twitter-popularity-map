5a4b257445093fbd1fa450e71cc4bd78a2856436
Miklos Bowling <miklos.bowling@gmail.com>
2020-12-26 20:29:58 -0800

    Added new fields for tweet text in DB.

--- api/api.py
+++ api/api.py
@@ -1,16 +1,17 @@
+import nltk
+import requests
+from flask import Flask, request
+import datetime as dt
+import time
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import numpy as np
 from models import db, StateModel
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from constants import LIST_OF_STATES
 from searchtweets import ResultStream, gen_request_parameters, load_credentials, collect_results
 import pandas as pd
-import numpy as np
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import time
-import datetime as dt
-from flask import Flask
 
-import nltk
 nltk.download('vader_lexicon')
 vader = SentimentIntensityAnalyzer()
 
@@ -35,22 +36,30 @@ def get_current_time():
 
 def fetch_new_tweets():
     # TODO: Allow this to look for env variables
+    #
     search_args = load_credentials(
         "./.twitter_keys.yaml", yaml_key="search_tweets_v2", env_overwrite=False)
 
     resp_dict = dict()
     for state in LIST_OF_STATES:
-        qry_str = "entity:\"" + state + "\" -is:retweet"
-        query = gen_request_parameters(qry_str, results_per_call=10)
-        tweets = collect_results(query, max_tweets=10,
+        qry_str = "entity:\"" + state + "\" -is:retweet lang:en -has:links -has:mentions -context:11.689566306014617600 -context:11.706083902411055104 -context:11.769193663230468096"
+        query = gen_request_parameters(qry_str, results_per_call=50)
+        try:
+            tweets = collect_results(query, max_tweets=250,
                                  result_stream_args=search_args)
-        df = pd.DataFrame.from_dict(tweets[:-1])
-        df['scores'] = df['text'].apply(
-            lambda text: vader.polarity_scores(text)['compound'])
-        neg = int(df[df['scores'] < -0.1].count()['id'])
-        pos = int(df[df['scores'] > 0.1].count()['id'])
-        resp_dict[state] = (neg, pos)
-        print(df)
+            df = pd.DataFrame.from_dict(tweets)
+            df['scores'] = df['text'].apply(
+                lambda text: vader.polarity_scores(str(text))['compound'])
+            neg = int(df[df['scores'] < -0.1].count()['id'])
+            neg_tweet = df[df['scores'] ==
+                           df['scores'].min()].iloc[0]["text"]
+            pos = int(df[df['scores'] > 0.1].count()['id'])
+            pos_tweet = df[df['scores'] ==
+                           df['scores'].max()].iloc[0]["text"]
+            resp_dict[state] = (neg, pos, neg_tweet, pos_tweet)
+            print(df)
+        except requests.exceptions.HTTPError as e:
+            print(e)
 
     return resp_dict
 
@@ -58,12 +67,21 @@ def fetch_new_tweets():
 @app.route('/api/tweets')
 def get_tweets():
     # res = fetch_new_tweets()
-    res = {"California": (0, 1)}
-    for state, scores in res.items():
-        new_entry = StateModel(state=state, date_retrieved=dt.datetime.now(
-        ), neg_score=scores[0], pos_score=scores[1])
-        db.session.add(new_entry)
-        db.session.commit()
+    date = dt.date.today()
+
+    res = dict()
+    for state_dat in db.session.query(StateModel):
+        if (date - state_dat.date_retrieved).days == 0:
+            res[state_dat.state] = (
+                state_dat.neg_score, state_dat.pos_score, state_dat.neg_tweet, state_dat.pos_tweet)
+
+    if len(res) == 0:
+        res = fetch_new_tweets()
+        for state, scores in res.items():
+            new_entry = StateModel(state=state, date_retrieved=dt.datetime.now(
+            ), neg_score=scores[0], pos_score=scores[1], neg_tweet=scores[2], pos_tweet=scores[3])
+            db.session.add(new_entry)
+            db.session.commit()
 
     return res
 
--- api/models.py
+++ api/models.py
@@ -11,12 +11,16 @@ class StateModel(db.Model):
     date_retrieved = db.Column(db.Date())
     pos_score = db.Column(db.Integer())
     neg_score = db.Column(db.Integer())
+    pos_tweet = db.Column(db.String())
+    neg_tweet = db.Column(db.String())
 
-    def __init__(self, state, date_retrieved, neg_score, pos_score):
+    def __init__(self, state, date_retrieved, neg_score, pos_score, neg_tweet, pos_tweet):
         self.state = state
         self.date_retrieved = date_retrieved
         self.neg_score = neg_score
         self.pos_score = pos_score
+        self.neg_tweet = neg_tweet
+        self.pos_tweet = pos_tweet
 
     def __repr__(self):
         return f"{self.state} on {self.date_retrieved}: ({self.pos_score}, {self.neg_score})"
