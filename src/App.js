264f3d96ad81fcfcf92785b863d7af07c613146c
Miklos Bowling <miklos.bowling@gmail.com>
2021-01-02 22:08:35 -0800

    Fixed the text below the map

--- src/App.js
+++ src/App.js
@@ -26,22 +26,12 @@ function App() {
           <MapChart tweet_type={type} tweet_date={date}/>
         </div>
         <p>
-          To create this site, I ran a sentiment analysis on a random sample of 200
-          tweets published in the last 7 days for each state in the United States.
-          I determined if a particular tweet was about a particular US state by using the Twitter
-          API's new <em>Annotations</em> feature, which uses AI to determine if
-          a tweet is discussion a particular topic. Every day, I repeated this analysis to try to get an idea of how sentiment about states changed
-          over time. You can observe this change by adjusting the date slider.
-          I also decided to judge each state by tweets that showed a favorable
-          bent, as well as an unfavorable one. I guessed that these two variables
-          wouldn't be perfect correlated, and was interested to see the results.
-          To see those results for yourself, you can toggle between the Favorable
-          and Unfavorable Tweets.
+          Use the slider on the left to select a date to poll tweets from, and use the right toggle to get positive or negative tweets.
         </p>
-        <p>
-          In the future, there are many features I'd like to add. I would list them,
-          but I am tired. Apologies for the wall of text.
-        </p>
+        // <p>
+        //   In the future, there are many features I'd like to add. I would list them,
+        //   but I am tired. Apologies for the wall of text.
+        // </p>
       </div>
     </div>
   );
